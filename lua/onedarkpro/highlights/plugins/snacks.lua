21b3a7f97e2dde3c7d26ed1f3a34f946514bb7f8
Qeuroal <Qeuroal@163.com>
2025-04-28 12:09:35 +0800

    feat: update highlights and gitignore

--- lua/onedarkpro/highlights/plugins/snacks.lua
+++ lua/onedarkpro/highlights/plugins/snacks.lua
@@ -4,11 +4,19 @@ local M = {}
 ---@param theme table
 ---@return table
 function M.groups(theme)
-    return {
+    local highlights = {
         -- https://github.com/folke/snacks.nvim
         SnacksIndent = { fg = theme.generated.indentline },
         SnacksIndentScope = { fg = theme.palette.blue },
+        SnacksPickerCol = { fg = theme.generated.line_number },
+        SnacksPickerDir = { link = "Text" },
+        SnacksPickerBufFlags = { link = "SnacksPickerDir" },
     }
+    for i, color in ipairs(theme.rainbow) do
+        highlights["SnacksIndentScope" .. i] = { fg = color }
+    end
+
+    return highlights
 end
 
 return M
